2a337262502927e1a3f0dcec1bcb9dab115f5c82
putcode677 <putcode701@gmail.com>
2026-05-18 23:54:33 +0300

    first javascript

---
+++ app.js
@@ -0,0 +1,26 @@
+//DOM Manipulation
+
+
+
+//getElemetById()
+const title  = document.getElementById('main-heading');
+
+console.log(title);
+
+
+// GetElementByclassName()
+const listItem= document.getElementsByClassName('list-items')
+
+
+console.log(listItem)
+
+
+
+
+//get ElementByTagName()
+
+
+//querySelector()
+
+
+//querySelectorAll()
